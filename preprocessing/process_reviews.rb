f9b672f28c7e13fd4a921d3ef5a677decf1f7a3b
isaac <isaac@isaac-AOD255.(none)>
2015-03-23 18:07:06 -0400

    Changed CSV column separator.

--- preprocessing/process_reviews.rb
+++ preprocessing/process_reviews.rb
@@ -1,9 +1,11 @@
 require 'json'
 require 'csv'
 
+# To run: ruby process_reviews.rb ...yelp_academic_dataset_review.json ...review.csv
+
 # Returns the JSON from :file.
 def process_file(file1, file2)
-  CSV.open(file2, 'w') do |csv|
+  CSV.open(file2, 'w', {:col_sep => "\t"}) do |csv|
   File.open(file1, "r").each_line do |line|
     x = JSON.parse(line)
     y = {}
@@ -11,7 +13,7 @@ def process_file(file1, file2)
     y['business_id'] = x['business_id']
     y['stars'] = x['stars']
     y['date'] = x['date']
-    y['text'] = x['text'].gsub("\n", ' ')
+    y['text'] = x['text'].gsub("\n", ' ').gsub("\t", ' ')
     csv << y.values
   end
   end
