e5e9db07fe937f889ce26a7a988097ab08b70fd4
Rebecca <rebstar6@gmail.com>
2015-03-24 15:38:24 -0400

    attributes code

--- preprocessing/Attributes.py
+++ preprocessing/Attributes.py
@@ -2,33 +2,78 @@
 import json
 import csv
 import collections
-import numpy as np
+
 
 
 bizpath = '../../yelp_dataset/yelp_academic_dataset_business.json'
 
 
-resultFile = open("busCat.csv",'w')
+resultFile = open("busAttr.csv",'w')
 wr = csv.writer(resultFile)
 
-#biz_cat = {}
-attrOpts = collections.Counter()
-attrOptsBool = collections.Counter()
-attrOptsDig = collections.Counter()
-attrOptsUC = collections.Counter()
-attrOptsDict = collections.Counter()
 
-priceOpts = set()
-ambianceOpts = set()
-alcOpts = set()
+mappings = {u'Accepts Credit Cards':1, 
+u'Caters':2, 
+u'Delivery':3, 
+u'Good For Dancing':4,
+u'Good For Groups':5,
+u'Good for Kids':6,
+u'Good For Kids':6,
+u'Outdoor Seating':7,
+u'Take-out':8,
+u'Takes Reservations':9,
+u'Waiter Service':10,
+u'Wheelchair Accessible':11,
+u'Alcohol':12,
+u'Parking':13,
+u'Wi-Fi':14
+}
 
-count = 0
+ambianceMappings = {u'casual':20,
+u'classy':21,
+u'divey':22,
+u'hipster':23,
+u'intimate':24,
+u'romantic':25,
+u'touristy':26,
+u'trendy':27,
+u'upscale':28
+}
+
+attireMappings = {u'casual':30,
+u'dressy':31,
+u'formal':32
+}
+
+goodForMappings = {
+u'breakfast':40,
+u'brunch':41,
+u'dessert':42,
+u'dinner':43,
+u'latenight':44,
+u'lunch':45
+}
+
+noiseMappings = {
+u'average':50,
+u'quiet':51,
+u'loud':52,
+u'very_loud':53,
+}
+
+priceMappings = {
+1:60,
+2:61,
+3:62,
+4:63,
+}
+
 with open(bizpath) as biz_data:
     for line in biz_data:
 
         json_biz = json.loads(line)
         businessId = json_biz['business_id'].encode('utf-8','replace')
-        
+
 
         attrAll = json_biz["attributes"]
         for attr in attrAll:
@@ -36,40 +81,42 @@ with open(bizpath) as biz_data:
             objType = type(attrAll[attr])
 
             if objType == bool:
-                attrOptsBool[attr] = attrOptsBool[attr] + 1
-            elif objType == unicode:
-                #u'Alcohol': 20457, u'Attire': 20390, u'Noise Level': 18519, u'Wi-Fi': 17898
-                #all others low
-                if(attr == u'Alcohol'):
-                    alcOpts.add(attrAll[attr])
-                attrOptsUC[attr] = attrOptsUC[attr] + 1
-            elif objType == int: 
-                #price range only
-                attrOptsDig[attr] = attrOptsDig[attr] + 1
-                priceOpts.add(attrAll[attr])
-            elif objType == dict:
-                #u'Parking': 37967, u'Good For': 19288, u'Ambience': 18547
-                #all the rest have low counts anyway
-                attrOptsDict[attr] = attrOptsDict[attr] + 1
-                if(attr == u'Ambience'):
-                    for opt in attrAll[attr]:
-                        ambianceOpts.add(opt)
-            else:
-                print attr
+                if(attr in mappings):
+                    wr.writerow([businessId, mappings[attr]])
+            #num/unicode types
+            elif attr ==  u'Alcohol':
+                #convert to bool
+                if(attrAll[attr] == u'beer_and_wine'):
+                    wr.writerow([businessId, mappings[attr]])
+                elif(attrAll[attr] == u'full_bar'):
+                    wr.writerow([businessId, mappings[attr]])
+            elif attr ==  u'Noise Level':
+                wr.writerow([businessId, noiseMappings[attrAll[attr]]])
+            elif attr ==  u'Parking':
+                #convert to bool
+                wr.writerow([businessId, mappings[attr]])
+            elif attr ==  u'Attire':
+                wr.writerow([businessId, attireMappings[attrAll[attr]]])
+            elif attr ==  u'Price Range':
+                wr.writerow([businessId, priceMappings[attrAll[attr]]])
+            elif attr ==  u'Wi-Fi':
+                 #convert to bool
+                if(attrAll[attr] == u'free'):
+                    wr.writerow([businessId, mappings[attr]])
+                elif(attrAll[attr] == u'paid'):
+                    wr.writerow([businessId, mappings[attr]])
+            
+            #dictionary types
+            elif attr ==  u'Good For':
+                for gf in attrAll[attr]:
+                    if attrAll[attr][gf]:
+                        wr.writerow([businessId, goodForMappings[gf]])
+            elif attr ==  u'Ambience':
+                for gf in attrAll[attr]:
+                    if attrAll[attr][gf]:
+                        wr.writerow([businessId, ambianceMappings[gf]])
 
-            attrOpts[attr] = 1
 
-
-
-print len(attrOpts)
-print len(attrOptsBool)
-print len(attrOptsDig)
-print len(attrOptsUC)
-print len(attrOptsDict)
-
-print priceOpts
-print ambianceOpts
-print alcOpts
 
 
 
