1d03af78967cc557dfef92606cb3dbfd8aed0d1d
Rebecca <rebstar6@gmail.com>
2015-04-08 21:35:44 -0400

    BC edit

--- app/app/models/businesscategory.rb
+++ app/app/models/businesscategory.rb
@@ -2,6 +2,6 @@ class Businesscategory < ActiveRecord::Base
 	self.table_name = 'ORACLEMASTER.BUSINESSCATEGORIES'
   attr_accessible :business_id, :category_id
 
-  belongs_to :businesses
-  belongs_to :categories
+  belongs_to :businesses, :foreign_key => "business_id"
+  belongs_to :categories, :foreign_key => "category_id"
 end
