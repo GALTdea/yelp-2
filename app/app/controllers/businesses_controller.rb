40ca4e8473b09d02f4bdb9030dee71f0adeb9524
Rebecca <rebstar6@gmail.com>
2015-04-23 17:15:21 -0400

    updates to biz controller for queries

--- app/app/controllers/businesses_controller.rb
+++ app/app/controllers/businesses_controller.rb
@@ -1,9 +1,20 @@
 class BusinessesController < ApplicationController
 
   def index
-    @b = Business.where(:city => params[:city]).limit(100)
+  #  @cat = Businesscategory.where(:category_id => params[:categories])
 
-    @businesses = @b.each {|x| x['rating'] = 1}
+    @businesses = Business.where(:city => params[:city])
+
+    if(params.has_key?(:categories))
+    	@businesses = @businesses.joins(:businesscategories).where('businesscategories.category_id' => params[:categories])
+    end
+    if(params.has_key?(:atributes))
+    	@businesses = @businesses.joins(:businessattributes).where('businessattributes.attribute_id' => params[:attributes])
+    end
+
+   # Business.joins(:categories)
+
+  # @businesses = @b.each {|x| x[:rating] = 1}
 
     respond_to do |format|
       format.html # index.html.erb
--- app/app/models/business.rb
+++ app/app/models/business.rb
@@ -1,6 +1,6 @@
 class Business < ActiveRecord::Base
 	self.table_name = 'oraclemaster.businesses'
-  attr_accessible :business_id, :city, :full_address, :latitude, :longitude, :name, :state
+  attr_accessible :business_id, :city, :full_address, :latitude, :longitude, :name, :state, :rating
 
   has_many :businesscategories, :foreign_key  => "business_id"
   has_many :categories, through: :businesscategories
@@ -9,4 +9,9 @@ class Business < ActiveRecord::Base
   has_many :zattributes, through: :businessattributes
 
   has_many :reviews, :foreign_key  => "business_id"
+before_validation :set_rating
+ def set_rating
+    return self.rating = 1
+  end
+
 end
