29868ee11588b28b08eb80f5d708857b4bc645eb
Rebecca <rebstar6@gmail.com>
2015-04-23 13:51:21 -0400

    more zattributes

--- app/app/models/attribute.rb
+++
@@ -1,7 +0,0 @@
-class Attribute < ActiveRecord::Base
-	self.table_name = 'ORACLEMASTER.ATTRIBUTES'
-  attr_accessible :attribute_id, :attribute_name
-
-    has_many :businessattributes, :foreign_key  => "attribute_id"
-  has_many :businesses, through: :businessattributes
-end
