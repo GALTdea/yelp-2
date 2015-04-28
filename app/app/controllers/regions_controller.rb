d1f232a6727af969fa32cacd2bc6fe863593cb45
Rebecca <rebstar6@gmail.com>
2015-04-28 19:06:40 -0400

    adding regions?

---
+++ app/app/controllers/regions_controller.rb
@@ -0,0 +1,12 @@
+class RegionsController < ApplicationController
+
+  def index
+    @regions = Region.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @regions }
+    end
+  end
+
+end
---
+++ app/app/models/regions.rb
@@ -0,0 +1,5 @@
+class Region < ActiveRecord::Base
+  self.table_name = 'ORACLEMASTER.REGIONS'
+  attr_accessible :region
+
+end
